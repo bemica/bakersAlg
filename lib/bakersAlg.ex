2f06c639288821607279bc39f29524343bef3c2e
nathan-beneke <benek020@morris.umn.edu>
2018-04-05 13:35:34 -0500

    Create files for basic setup

---
+++ lib/bakersAlg.ex
@@ -0,0 +1,18 @@
+defmodule BakersAlg do
+  @moduledoc """
+  Documentation for BakersAlg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BakersAlg.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

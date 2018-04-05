056c940f05e6521a4fe36225dfbcf0f7b01bdc3b
nathan-beneke <benek020@morris.umn.edu>
2018-04-05 13:48:06 -0500

    Start writing manager.ex

---
+++ lib/bakery/manager.ex
@@ -0,0 +1,11 @@
+defmodule Manager do
+  def open_store(num_customers, num_servers) do
+    customers = #TODO
+    servers = #TODO
+    start_serving(customers, servers)
+  end
+
+  def start_serving(customers, servers) do
+    #TODO
+  end
+end
